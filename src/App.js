a79d7902e53a03679131b743b0d0f4b59482a478
Mainul Dip <mainuldip@gmail.com>
2021-04-16 01:33:35 +0600

    Global Contex Created NOBugs

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import IncomeExpenses from './components/IncomeExpenses'
 import TransactionList from './components/TransactionList'
 import AddTransaction from './components/AddTransaction'
 
+import { GlobalProvider } from './context/GlobalState'
+
 function App () {
   return (
-    <div className=''>
+    <GlobalProvider>
       <Header />
       <div className='container'>
         <Balance />
@@ -14,7 +16,7 @@ function App () {
         <TransactionList />
         <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   )
 }
 
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,5 +1,5 @@
 import React, { createContext, useReducer } from 'react'
-
+import AppReducer from './AppReducer'
 //initial state
 const initialState = {
   transactions: [
@@ -12,3 +12,35 @@ const initialState = {
 
 // Create Context
 export const GlobalContext = createContext(initialState)
+
+// Provider
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState)
+
+  //Actions
+  function deleteTransaction (id) {
+    dispatch({
+      type: 'DELETE_TRANSACTION',
+      payload: id
+    })
+  }
+
+  function addTransaction (newTransaction) {
+    dispatch({
+      type: 'ADD_TRANSACTION',
+      payload: newTransaction
+    })
+  }
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  )
+}
